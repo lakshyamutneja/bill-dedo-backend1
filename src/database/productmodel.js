299bf2d3bd5c0cef8e610a1281fd029f3ba03f57
roneyyb <bothra.rajat08@gmail.com>
2020-05-21 16:21:09 +0530

    UI overhaul

--- src/database/productmodel.js
+++ src/database/productmodel.js
@@ -3,7 +3,12 @@ var productSchema = mongoose.Schema({
     name: { type: String },
     image: { type: String },
     price: { type: String },
-    id: { type: String },
+    id: { 
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
     desc: { type: String },
     expiryDate: String
 });
--- src/route/product.js
+++ src/route/product.js
@@ -1,42 +1,48 @@
 const express = require("express");
 const router = new express.Router();
 const Product = require("../database/productmodel");
-   
 
-
-router.post('/addProduct', (req, res) => {
-    //console.log(req);
-    var name = req.body.name,
-        image = req.body.image,
-        price = req.body.price,
-        id = req.body.id,
-        desc = req.body.desc,
-        expiryDate = req.body.expiryDate;
-    var newProduct = { name, image, price, id, desc, expiryDate };
-    Product.create(newProduct, (err, succ) => {
-        if (err) res.status(400).json({ error: err });
-        else res.status(200).json({ success: "Product Created" });
-    });
+router.post("/addProduct", (req, res) => {
+  console.log(req.body);
+  var name = req.body.name,
+    image = req.body.image,
+    price = req.body.price,
+    id = req.body.id,
+    desc = req.body.desc,
+    expiryDate = req.body.expiryDate;
+  var newProduct = { name, image, price, id, desc, expiryDate };
+  Product.create(newProduct, (err, succ) => {
+    if (err) {
+      res.status(400).json({ message: "Barcode already exist." });
+    } else res.status(200).json({ success: "Product Created" });
+  });
 });
 
 router.get("/getProduct/:id", (req, res) => {
-    var id = req.params.id;
-    Product.findOne({ id })
-        .then(product => {
-            console.log(product);
-            res.status(200).json({ status: 200, product });
-        })
-        .catch(err => {
-            res.status(400).json({ error: err });
-        });
+  var id = req.params.id;
+  Product.findOne({ id })
+    .then((product) => {
+      console.log(product);
+      if (product) {
+        return res.status(200).json({ status: 200, product });
+      } else {
+        return res.status(400).json({ message: "Item Does Not Exist!!" });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json({ message: "Problem occured in server!!" });
+    });
 });
 
-router.get("/getAll",(req,res)=>{
-    Product.find().then((products)=>{
-        res.status(200).json({status:200, products});
-    }).catch((err)=>{
-        res.status(400).json({ error: err });
+router.get("/getAll", (req, res) => {
+  Product.find()
+    .then((products) => {
+      res.status(200).json({ status: 200, products });
     })
-})
+    .catch((err) => {
+      res.status(400).json({ error: err });
+    });
+});
 
 module.exports = router;
